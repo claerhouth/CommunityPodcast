215a962b51e17f47b59402bca81ed14bc7fe54d5
claerhouth <hclleke@hotmail.com>
2013-09-29 16:58:01 +0200

    URL aangepast

--- compod/compod/vendor/apipack/eden/src/config/config.php
+++ compod/compod/vendor/apipack/eden/src/config/config.php
@@ -14,7 +14,7 @@ return array(
         'Facebook'=> array(
           'appkey'=>'638413639522476',
           'appsecret'=>'3c45804cc85d4c5e98c76c4aa56d850d',
-          'redirecturl'=>'http://localhost:81/compod/compod/server.php/loginFacebook'
+          'redirecturl'=>'http://communitypodcast.azurewebsites.net/compod/compod/server.php/loginFacebook'
         ),
         'Instagram'=>array(
           'clientid'=>'',
